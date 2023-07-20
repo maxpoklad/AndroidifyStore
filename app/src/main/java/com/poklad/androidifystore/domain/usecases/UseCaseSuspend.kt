8a4c4d5ae0de43e76a1bd8ff38b7354f86cf7d56
MAX <grec99088@gmail.com>
2023-07-20 15:50:57 +0300

    refactoring useCase interfaces

--- app/src/main/java/com/poklad/androidifystore/domain/usecases/UseCaseSuspend.kt
+++ app/src/main/java/com/poklad/androidifystore/domain/usecases/UseCaseSuspend.kt
@@ -1,5 +1,5 @@
 package com.poklad.androidifystore.domain.usecases
 
-interface BaseUseCaseCoroutines<in Parameter, out Result> {
+interface UseCaseSuspend<in Parameter, out Result> {
     suspend fun execute(params: Parameter): Result
 }
