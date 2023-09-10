38641044e0e780d58f5d28f603ae469648985a4d
Maxim Poklad <grec99088@gmail.com>
2023-09-10 12:11:03 +0300

    fix: anim in splash screen

--- app/src/main/java/com/poklad/androidifystore/presentation/ui/screens/splash/SplashFragment.kt
+++ app/src/main/java/com/poklad/androidifystore/presentation/ui/screens/splash/SplashFragment.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.core.app.ActivityOptionsCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.poklad.androidifystore.R
 import com.poklad.androidifystore.databinding.FragmentSplashBinding
 import com.poklad.androidifystore.presentation.MainActivity
 import com.poklad.androidifystore.presentation.ui.base.BaseFragment
@@ -36,6 +38,11 @@ class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>() {
     private fun launchMainScreen() {
         val intent = Intent(requireContext(), MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        startActivity(intent)
+        val options = ActivityOptionsCompat.makeCustomAnimation(
+            requireContext(),
+            R.anim.fade_in,
+            R.anim.fade_out
+        ).toBundle()
+        startActivity(intent, options)
     }
 }
